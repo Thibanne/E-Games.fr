4bf3fe942711d724da64e68cbc4bb4edea135de6
Thibanne <43335635+Thibanne@users.noreply.github.com>
2018-10-19 14:09:12 +0200

    fusion Partie Robin + Adrien -> Master

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,22 +1,6 @@
-Var itemincart = 0;
-Var Shoplist = 0;
+// partie Adrien
 
-$(function(){
-<<<<<<< HEAD
 // popover function bootstrap
-  $('[data-toggle="popover"]').popover();
-=======
-  $(".cart").on("click" function(){
-    i=0;
-    $('#btnplus').click(function() {
-      i++;
-      $('#click').html(i);
-    });
-    $('#btnminus').click(function() {
-      i--;
-      $('#click').html(i);
-    });
-  });
+$('[data-toggle="popover"]').popover();
 
->>>>>>> 67b47c1af4054fb16b84fc61f44f65806b0fd05f
-});
+// fin partie Adrien
