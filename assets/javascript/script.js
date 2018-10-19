67b47c1af4054fb16b84fc61f44f65806b0fd05f
Thibanne <43335635+Thibanne@users.noreply.github.com>
2018-10-19 13:44:22 +0200

    Fusion partie Robin -> Master

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,17 @@
+Var itemincart = 0;
+Var Shoplist = 0;
+
 $(function(){
+  $(".cart").on("click" function(){
+    i=0;
+    $('#btnplus').click(function() {
+      i++;
+      $('#click').html(i);
+    });
+    $('#btnminus').click(function() {
+      i--;
+      $('#click').html(i);
+    });
+  });
 
 });
