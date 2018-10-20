2cbb6f125a479ca2ad530bae6b680eb7729dd051
itkg-nanne <nicolas.anne@businessdecision.com>
2018-10-20 16:06:32 +0200

    Update script.js

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -5,19 +5,33 @@ $(function(){
   $('[data-toggle="popover"]').popover();
 
   // fin partie Adrien
+
+
+//    nombre d'unité'
+//    plusieurs fois le même élement
+
     $('.Abutton').on('click', function(event) {
       var $card = $(this).parents('.card').eq(0);
       var $image = $('.card-image img', $card).clone().css({maxWidth: '20px', maxHeight: '20px'});
       var title = $('.card-title button', $card).html();
 
       var $table = $('.modal-body table')
-      if ($table.length = 0) {
+      if ($table.length == 0) {
           $table = $('<table>')
           $('.modal-body').append($table);
       }
       var $tr = $('<tr>');
       $tr.append($('<td>').append($image));
       $tr.append($('<td>').append(title));
-      $('.modal-body').append($tr);
+      $tr.append($('<td>').append(
+          $('<button>')
+              .html('X')
+              .addClass('remove-from-basket')
+      ));
+      $table.append($tr);
+    });
+
+    $(document).on('click', '.remove-from-basket', function(event) {
+      $(this).parents('tr').eq(0).remove();
     });
 });
