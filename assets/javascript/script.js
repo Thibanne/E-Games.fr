e9cdc3c082b525542f6200d045c0bdaffaeba6f0
FunkAdri <adrienhenry.fp@gmail.com>
2018-10-22 16:07:17 +0200

    oui

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -7,6 +7,34 @@ $(function(){
   // fin partie Adrien
 
  // ajout dans le Panier
+ var itemCount = 0;
+
+$(".add").click(function (){
+  itemCount ++;
+  $("#itemCount").html(itemCount).css("display", "block");
+  $(this).siblings(".itemDetails").clone().appendTo( "#cartItems" ).append('<button class="removeItem">Remove Item</button>');
+});
+
+$(".clear").click(function() {
+  itemCount = 0;
+  $("#itemCount").html('').css("display", "none");
+  $("#cartItems").html('');
+});
+
+$("i").click(function(){
+  $('#shoppingCart').toggle();
+});
+
+$("#shoppingCart").on("click", ".removeItem", function(){
+    $(this).parent().remove();
+    itemCount --;
+    $("#itemCount").html(itemCount);
+
+    if (itemCount === 0) {
+      $("#itemCount").html('').css("display", "none");
+      $("#shoppingCart").css("display", "none");
+    }
+});
  // retirer du Panier
  // nombre d'unité
  // plusieur le meme éléments
